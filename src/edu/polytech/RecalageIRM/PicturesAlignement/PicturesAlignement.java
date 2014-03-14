6848ffdea7e554d21d7e7b265dc9495743bb27bc
Samantha <Samantha@172.25.1.225>
2014-03-14 15:29:00 +0100

    Creation Classe PicturesAlignement

---
+++ src/edu/polytech/RecalageIRM/PicturesAlignement/PicturesAlignement.java
@@ -0,0 +1,20 @@
+package edu.polytech.RecalageIRM.PicturesAlignement;
+
+
+import ij.ImageStack;
+
+
+
+public class PicturesAlignement {
+
+	//Attributes
+
+	//Constructor
+	public PicturesAlignement(ImageStack FirstImageStack, ImageStack SecondImageStack){
+
+	}
+
+	
+	
+}
+
